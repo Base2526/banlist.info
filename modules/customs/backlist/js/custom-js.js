0180e752d5d943b28479aeaf6153af73e5d88f86
Somkid <mr.simajarn@gmail.com>
2020-12-26 14:20:50 +0700

    add themes && js

---
+++ modules/customs/backlist/js/custom-js.js
@@ -0,0 +1,6 @@
+(function($, Drupal, drupalSettings){
+    console.log('#1');
+    $(document).ready(function() {
+        console.log('custom-js.js hihi');
+    });
+})(jQuery, Drupal, drupalSettings);
