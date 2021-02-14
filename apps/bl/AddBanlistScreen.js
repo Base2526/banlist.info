2562759333c89dc5d11a1e2ba7e3fa064f4429bb
somkid simajarn <somkidsimajarn@somkids-MacBook-Pro.local>
2021-02-14 08:34:06 +0700

    before add npx react-codemod rename-unsafe-lifecycles

--- apps/bl/AddBanlistScreen.js
+++ apps/bl/AddBanlistScreen.js
@@ -17,21 +17,22 @@ import {
   TouchableOpacity,
   TextInput,
   ActivityIndicator,
-  Button
+  Button,
+  Image,
 } from 'react-native';
 
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
+import {
+  Header,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
 
 // import DatePicker from 'react-native-datepicker'
 import DropDownPicker from 'react-native-dropdown-picker';
 import Icon from 'react-native-vector-icons/Feather';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import ImagePicker from 'react-native-image-crop-picker';
+import ActionSheet from 'react-native-actionsheet';
 
 const axios = require('axios');
 var Buffer = require('buffer/').Buffer
@@ -49,11 +50,21 @@ class AddBanlistScreen extends Component {
                 
                 
                 
-                  textInput : []};
+                  textInput : [],
+                
+                  selectedPhotoIndex: 0,
+                  localPhotos: []};
   
     // const [date, setDate] = useState(new Date(1598051730000));
     // const [mode, setMode] = useState('date');
     // const [show, setShow] = useState(false);
+
+    // this.state = {
+    //   logs: 'ahihi',
+    //   selectedPhotoIndex: 0,
+    //   localPhotos: []
+    // };
+    this.onDoUploadPress = this.onDoUploadPress.bind(this);
 
   }
 
@@ -118,10 +129,111 @@ class AddBanlistScreen extends Component {
     this.setState({ textInput })
   }
 
-  /*
-  <Text>เลขบัญชี</Text>
-  
-  */
+  // Upload image
+  onPressAddPhotoBtn = () => {
+    this.ActionSheetSelectPhoto.show();
+  };
+
+  showActionSheet = index => {
+    this.setState({
+      selectedPhotoIndex: index
+    });
+    this.ActionSheet.show();
+  };
+
+  onDoUploadPress() {
+    const { localPhotos } = this.state;
+    if (localPhotos && localPhotos.length > 0) {
+      let formData = new FormData();
+      localPhotos.forEach((image) => {
+        const file = {
+          uri: image.path,
+          name: image.filename || Math.floor(Math.random() * Math.floor(999999999)) + '.jpg',
+          type: image.mime || 'image/jpeg'
+        };
+        formData.append('files', file);
+      });
+
+      axios
+        .post('https://api.tradingproedu.com/api/v1/fileupload', formData)
+        .then(response => {
+          this.setState({ logs: JSON.stringify(response.data) });
+        })
+        .catch(error => {
+          alert(JSON.stringify(error));
+        });
+    } else {
+      alert('No photo selected');
+    }
+  }
+
+  onActionDeleteDone = index => {
+    if (index === 0) {
+      const array = [...this.state.localPhotos];
+      array.splice(this.state.selectedPhotoIndex, 1);
+      this.setState({ localPhotos: array });
+    }
+  };
+
+  onActionSelectPhotoDone = index => {
+    switch (index) {
+      case 0:
+        ImagePicker.openCamera({}).then(image => {
+          this.setState({
+            localPhotos: [...this.state.localPhotos, image]
+          });
+        });
+        break;
+      case 1:
+        ImagePicker.openPicker({
+          multiple: true,
+          maxFiles: 10,
+          mediaType: 'photo'
+        }).then(images => {
+          images.forEach((image) => {
+            this.setState({
+              localPhotos: [...this.state.localPhotos, image]
+            });
+          });
+        }).catch(error => {
+          alert(JSON.stringify(error));
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  renderListPhotos = localPhotos => {
+    const photos = localPhotos.map((photo, index) => (
+      <TouchableOpacity key={index}
+        onPress={() => {
+          this.showActionSheet(index);
+        }}
+      >
+        <Image style={styles.photo} source={{ uri: photo.path }} />
+      </TouchableOpacity>
+    ));
+
+    return photos;
+  };
+
+  renderSelectPhotoControl = localPhotos => {
+    return (
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Select photos</Text>
+        <ScrollView style={styles.photoList} horizontal={true}>
+          {this.renderListPhotos(localPhotos)}
+          <TouchableOpacity onPress={this.onPressAddPhotoBtn.bind(this)}>
+            <View style={[styles.addButton, styles.photo]}>
+              <Text style={styles.addButtonText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    );
+  };
+  // Upload image
   
   render(){
     return (
@@ -222,7 +334,52 @@ class AddBanlistScreen extends Component {
                 return value
               })}
 
-      
+              {/*  */}
+              <SafeAreaView>
+              <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={styles.scrollView}>
+                {/* <Header /> */}
+                {/* <Image style={{ width: 143, height: 30 }} source={{ uri: 'https://tuanitpro.com/wp-content/uploads/2015/04/logo.png' }} /> */}
+                <View style={styles.body}>
+                  {this.renderSelectPhotoControl(this.state.localPhotos)}
+                  <View style={styles.sectionContainer}>
+                    <TouchableOpacity onPress={this.onDoUploadPress}>
+                      <Text style={styles.sectionTitle}>Upload now</Text>
+                    </TouchableOpacity>
+                  </View>
+                  {/* <View style={styles.sectionContainer}>
+                    <Text style={styles.sectionTitle}>Logs</Text>
+                    <TextInput multiline numberOfLines={10} style={{ height: 250, borderColor: 'gray', borderWidth: 1 }}
+                      value={this.state.logs}
+                    />
+                  </View> */}
+                </View>
+              </ScrollView>
+              {/*  */}
+
+              <ActionSheet
+                ref={o => (this.ActionSheet = o)}
+                title={'Confirm delete photo'}
+                options={['Confirm', 'Cancel']}
+                cancelButtonIndex={1}
+                destructiveButtonIndex={0}
+                onPress={index => {
+                  this.onActionDeleteDone(index);
+                }}/>
+
+              <ActionSheet
+                ref={o => (this.ActionSheetSelectPhoto = o)}
+                title={'Select photo'}
+                options={['Take Photo...', 'Choose from Library...', 'Cancel']}
+                cancelButtonIndex={2}
+                destructiveButtonIndex={1}
+                onPress={index => {
+                  console.log('ActionSheetSelectPhoto');
+                  this.onActionSelectPhotoDone(index);
+                }}/>
+              </SafeAreaView>
+
               <TouchableOpacity
                 style={styles.button}
                 onPress={this.handleAdd}>
